8c1256642a59633931bdd931cae05518b74c0e4b
Abhishek Kumar <hind.tigerabhisingh@gmail.com>
2020-06-20 14:19:12 +0530

    file structure added

---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from '../Layouts/NavBar'
+import Footer from '../Layouts/Footer'
+import MidSectionContainer from '../component/MiddleSection/MidSectionContainer'
+
+function Home() {
+    return (
+        <>
+        <NavBar />
+        <MidSectionContainer />
+        <Footer />
+        </>
+    )
+}
+
+export default Home
---
+++ src/component/MiddleSection/MidSectionContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class MidSectionContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default MidSectionContainer
