3314a5c9e632f4f1d400b8d69e158e44d082fb79
Abhishek Kumar <hind.tigerabhisingh@gmail.com>
2020-06-20 17:50:32 +0530

    middle section issue fix

--- src/component/MIdSectionContainer.jsx
+++ src/component/MIdSectionContainer.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { NewteamComp } from './NewteamComp'
 import SimpleBreadcrumbs from './SimpleBreadcrumbs'
 import MembersComp from './MembersComp'
+import { withStyles, Paper } from '@material-ui/core';
 
-export function MIdSectionContainer() {
-  return (
-     <>
-     <SimpleBreadcrumbs />
-     <NewteamComp />
-     <MembersComp />
-     </>
-  )
+const styles = theme => ({
+  root: {
+    marginRight: theme.spacing(5),
+    marginLeft: theme.spacing(5),
+  },
+
+});
+
+export class MIdSectionContainer extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <SimpleBreadcrumbs />
+        <NewteamComp />
+        <MembersComp />
+      </Paper>
+    )
+  }
 }
 
-export default MIdSectionContainer
+export default MIdSectionContainer = withStyles(styles, { withTheme: true })(MIdSectionContainer);
+
 
